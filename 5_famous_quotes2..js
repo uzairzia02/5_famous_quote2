9ce75989c3448cb5a57b8f9ede927b74e172c0f6
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 12:30:54 +0500

    initial commit

---
+++ 5_famous_quotes2..js
@@ -0,0 +1,4 @@
+"use strict";
+let message = '"Think 100 times before you take a decision, But once that decision is taken, stand by it as one man."';
+let famous_person = "Quaid E Azam";
+console.log(`${famous_person} once said, ${message}`);
---
+++ 5_famous_quotes2.ts
@@ -0,0 +1,4 @@
+let message: string = '"Think 100 times before you take a decision, But once that decision is taken, stand by it as one man."';
+let famous_person: string = "Quaid E Azam";
+
+console.log(`${famous_person} once said, ${message}`)
